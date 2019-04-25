ee769ebfd41483d885034d0ddbef28e01135a7c9
Abbey Dent <adent@g.clemson.edu>
2019-04-25 16:45:05 -0400

    using NaiveBayesAnalyzer

--- try_textblob.py
+++ try_textblob.py
@@ -2,6 +2,7 @@ from textblob import TextBlob
 from nltk.tokenize import TabTokenizer 
 from textblob import Word
 
+from textblob.sentiments import NaiveBayesAnalyzer
 
 # text_blob_object = TextBlob("Tom is great!")
 # print(text_blob_object.tags)
@@ -13,14 +14,24 @@ from textblob import Word
 # print(text_blob_object.words)
 # print(text_blob_object.sentences)
 
-word = Word("equilibria")
-print(word.lemmatize())
+# word = Word("equilibria")
+# print(word.lemmatize())
 
-text_blob_object = TextBlob("Thiss is correcct senetenc.")
-print(text_blob_object.correct())
+# text_blob_object = TextBlob("Thiss is correcct senetenc.")
+# print(text_blob_object.correct())
 #corrects "correcct" to "correct" and "senetenc" to "sentence". "Thiss" was corrected to "Hiss" rather than "This".
 
 
-text_blob_object = TextBlob("Tom is great! Liuna is even better.")
+text_blob_object = TextBlob("Tom is great! Liuna is even better.", analyzer = NaiveBayesAnalyzer())
 print(text_blob_object.sentiment)
+#This sentence is approximately 63% positive and 37% negative
+
+text_blob_object = TextBlob("This program is amazing.", analyzer=NaiveBayesAnalyzer())
+print(text_blob_object.sentiment)
+#This sentence is approximately 67% positive and 33% negative.
+
+text_blob_object = TextBlob("Baby Shark Do do.", analyzer = NaiveBayesAnalyzer())
+print(text_blob_object.sentiment)
+
+
 
