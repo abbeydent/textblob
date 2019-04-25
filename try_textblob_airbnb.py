1e7327f566cd41875e396461a8e4a80cd5872c87
Abbey Dent <adent@g.clemson.edu>
2019-04-25 17:08:06 -0400

    analyzing comments on airbnb

--- try_textblob_airbnb.py
+++ try_textblob_airbnb.py
@@ -3,8 +3,14 @@ from textblob.sentiments import NaiveBayesAnalyzer
 import pandas as pd
 import numpy as np 
 
-dataset = pd.read_csv("airbnb-reviews-part-3.csv")
+dataset = pd.read_csv("airbnb-reviews-part-3.csv", sep = ";")
 
 comments = dataset.iloc[0:500,5].values
 
-print(comments)
+print(comments[120])
+
+
+comment = TextBlob(comments[120], analyzer = NaiveBayesAnalyzer())
+print(comment)
+print(comment.tags)
+print(comment.sentiment)
