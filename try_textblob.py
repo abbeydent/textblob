2202f5f1fc7fedba52cbc7ab535b9d2366dbe29b
Abbey Dent <adent@g.clemson.edu>
2019-04-25 16:24:53 -0400

    using 'Word' from textblob

--- try_textblob.py
+++ try_textblob.py
@@ -1,13 +1,17 @@
 from textblob import TextBlob 
 from nltk.tokenize import TabTokenizer 
+from textblob import Word
 
 
+# text_blob_object = TextBlob("Tom is great!")
+# print(text_blob_object.tags)
 
-text_blob_object = TextBlob("Tom is great!")
-print(text_blob_object.tags)
 
-tokenizer = TabTokenizer()
-text_blob_object = TextBlob("Tom is great! Liuna is even better.", tokenizer = tokenizer)
-print(text_blob_object.tokens)
-print(text_blob_object.words)
-print(text_blob_object.sentences)
+# tokenizer = TabTokenizer()
+# text_blob_object = TextBlob("Tom is great! Liuna is even better.", tokenizer = tokenizer)
+# print(text_blob_object.tokens)
+# print(text_blob_object.words)
+# print(text_blob_object.sentences)
+
+word = Word("equilibria")
+print(word.lemmatize())
